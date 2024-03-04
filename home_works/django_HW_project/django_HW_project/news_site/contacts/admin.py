fda0de13b88305485235a141824fe7dc2eb151d0
Andy-hs79 <andy_hs@mail.ru>
2024-03-04 19:03:02 +0300

    update django project

--- home_works/django_HW_project/django_HW_project/news_site/contacts/admin.py
+++ home_works/django_HW_project/django_HW_project/news_site/contacts/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Name, Phone
 
+admin.site.register(Name)
+admin.site.register(Phone)
 # Register your models here.
--- home_works/django_HW_project/django_HW_project/news_site/contacts/models.py
+++ home_works/django_HW_project/django_HW_project/news_site/contacts/models.py
@@ -1,12 +1,18 @@
 from django.db import models
 
 
-class Phone_book(models.Model):
-    names = []
-    phones = models.OneToOneField(names)
+class Name(models.Model):
+    name = models.CharField(verbose_name='Имя', max_length=30, )
 
     def __str__(self):
-        return ...
-    
+        return self.name
+
+
+class Phone(models.Model):
+    phone = models.CharField(verbose_name='Телефон', max_length=10)
+    contact = models.ForeignKey(Name, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.phone
 
 # Create your models here.
