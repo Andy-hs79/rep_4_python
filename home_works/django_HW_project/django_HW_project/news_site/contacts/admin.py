85a9cb20726927b82ab27fea8988d8530f2d5622
Andy-hs79 <andy_hs@mail.ru>
2024-03-05 12:23:34 +0300

    add models book & author

--- home_works/django_HW_project/django_HW_project/news_site/contacts/admin.py
+++ home_works/django_HW_project/django_HW_project/news_site/contacts/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import Name, Phone
+from .models import Name, Phone, Book, Author
 
 admin.site.register(Name)
 admin.site.register(Phone)
+admin.site.register(Book)
+admin.site.register(Author)
+
 # Register your models here.
--- home_works/django_HW_project/django_HW_project/news_site/contacts/models.py
+++ home_works/django_HW_project/django_HW_project/news_site/contacts/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-
+# соотношение 1-ко-многим или "внешний ключ" (ForeignKey)
 class Name(models.Model):
     name = models.CharField(verbose_name='Имя', max_length=30, )
 
@@ -15,4 +15,18 @@ class Phone(models.Model):
     def __str__(self):
         return self.phone
 
+# соотношение многие-ко-многим (ManyToMany)
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    authors = models.ManyToManyField('Author')
+
+    def __str__(self):
+        return self.title
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
 # Create your models here.
